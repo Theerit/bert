d92be79609a22be42412eb79a3be05fa91a65490
theerit <theerit@gmail.com>
2018-12-04 21:21:18 +0700

    Dummy commit for run_squad.py

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -920,7 +920,7 @@ def main():
                     batch = tuple(t.to(device) for t in batch) # multi-gpu does scattering it-self
                 input_ids, input_mask, segment_ids, start_positions, end_positions = batch
                 loss = model(input_ids, segment_ids, input_mask, start_positions, end_positions)
-                loss_train['Epoch: '+str(ep) + 'Batch: ' + str(batch_count)] = loss
+                loss_train['Epoch: '+str(ep) + 'Batch: ' + str(batch_count)] = loss.item()
                 #loss_temp = loss
                 if n_gpu > 1:
                     loss = loss.mean() # mean() to average on multi-gpu.
